4dd8fbd2e9fc7278a8267443dc78e5afdd3ecee1
Javier Noris <norisjavier@gmail.com>
2014-04-28 15:34:07 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Krowdprize4::Application.config.secret_key_base = '326f1f118d0e39cada86eb2d75c515cd1a006534ee76adf81a6a4481b24391b38944a8f5a55183acab26b33076ed762beec354cab15cbffe1b827165323c2215'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Krowdprize4::Application.config.session_store :cookie_store, key: '_Krowdprize4_session'
